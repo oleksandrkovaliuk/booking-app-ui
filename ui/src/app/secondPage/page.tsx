0119c975addfaf75e88430312c0b7e9a2135e22a
Oleksandr Bilyi <o.d.bilyi@gmail.com>
2024-07-13 17:45:49 +0200

    hhh

---
+++ ui/src/app/secondPage/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@nextui-org/button";
+
+const SecondPage = () => {
+  return (
+    <Button color="primary" variant="solid">
+      Solid
+    </Button>
+  );
+};
+
+export default SecondPage;
---
+++ ui/src/layout/footer/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <p>
+        © 2024 Airbnb, Inc.·TermsSitemapPrivacyYour Privacy Choices Choose a
+      </p>
+      <p>language English (US) Choose a currency € EUR</p>
+    </footer>
+  );
+};

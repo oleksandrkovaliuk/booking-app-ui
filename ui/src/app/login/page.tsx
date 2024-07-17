07678f939f2c08f6b76768f89f5477fcc7dabd99
oleksandrkovaliuk <sasha1235qaz@ukr.net>
2024-07-17 16:23:47 -0600

    fixed(:production error with useSearchParams()

--- ui/src/app/login/page.tsx
+++ ui/src/app/login/page.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { Suspense } from "react";
 import { LoginModal } from "./modal";
 
 const LogInPage = () => {
-  return <LoginModal />;
+  return (
+    <Suspense fallback={null}>
+      <LoginModal />
+    </Suspense>
+  );
 };
 
 export default LogInPage;

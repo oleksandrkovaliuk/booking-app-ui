5a89abf6d138fba9ce4dae47ee47a4a69215a15a
oleksandrkovaliuk <sasha1235qaz@ukr.net>
2024-09-07 12:15:23 -0600

    changes(:home uninstall packages)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,14 +5,5 @@ import { HomeContent } from "./content";
 import { Roles } from "@/_utilities/enums";
 
 export default async function Home() {
-  const session = await getServerSession({
-    callbacks: {
-      session: async ({ session, token }) => {
-        if (token?.role) session.user.role = token.role as Roles;
-        return session;
-      },
-    },
-  });
-  console.log(session);
   return <HomeContent />;
 }
